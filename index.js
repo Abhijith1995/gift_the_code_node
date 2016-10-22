8636e63d2e66172d1d70f89db9b3d35bcf534ec5
Abhijith Ramalingam <abhijithramalingam1995@gmail.com>
2016-10-22 14:43:21 -0400

    Change port

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  // response.render('pages/index');
+  response.send("Hello, Abhijith");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
